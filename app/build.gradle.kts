cbc0ec4235d44d7a12a3fe81e074ce274356ac62
ZielinskiLukasz <lucas.zielinski@gmail.com>
2026-02-24 11:29:13 +0100

    bugfixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.ergonomic.mountainweather"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
